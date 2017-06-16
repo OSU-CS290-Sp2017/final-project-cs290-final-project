28f31c2366f38cdea0b5d3bc60db433a52938f4c
Bobi Kwon <kwonbo@flip3.engr.oregonstate.edu>
2017-06-16 14:17:42 -0700

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 var moviesData = require('./movieData');
 var app = express();
@@ -17,17 +18,48 @@ app.get('/movies', function (req, res, next) {
     name: movieData.name,
     discription: movieData.description
   };
-
   res.render('moviePage', templateArgs);
+});
 
+app.post('/movies/createMovieModal', function(req, res, next){
+
+  var movie = moviesData[req.params.movie];
+
+  if(movie){
+    if(req.body && req.body.picture && req.body.name && req.body.description){
+
+      var data ={
+        picture: req.body.picture,
+        name: req.body.name,
+        discription: req.body.description
+      };
+
+      movie.movies = movie.movies || [];
+      movie.movies.push(data);
+
+      fs.writeFile('movieData.json', JSON.stringify(moviesData), function (err)){
+        if (err) {
+           res.status(500).send("Unable to save movie to \"database\".");
+         } else {
+           res.status(200).send();
+         }
+       });
+     } else {
+  res.status(400).send("Movie information must have a URL.");
+}
+
+} else {
+next();
+}
 });
 
 app.use(express.static('public'));
 /**
-app.get('*', function (req, res) {
-  res.status(404).render('404Page');
-});
-**/
+ * app.get('*', function (req, res) {
+ *   res.status(404).render('404Page');
+ *   });
+ *   **/
 app.listen(port, function () {
   console.log("== Server listening on port", port);
-})
+});
+
